4cdaac2905c59be30e4cdad077b439ab27e4b1f5
valentin <vb.brebionpro@gmail.com>
2025-10-26 22:33:42 +0100

    add build vue

--- src/components/stepCont.vue
+++ src/components/stepCont.vue
@@ -2,16 +2,16 @@
    
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+// import { ref } from 'vue'
 
-const step = ref(0)
+// const step = ref(0)
 
-const steps = [
-  { label: 'Choose your product' },
-  { label: 'Checkout' },
-  { label: 'Review order' },
-  { label: 'Confirm and pay' },
-]
+// const steps = [
+//   { label: 'Choose your product' },
+//   { label: 'Checkout' },
+//   { label: 'Review order' },
+//   { label: 'Confirm and pay' },
+// ]
 </script>
 <style lang="scss">
 $primary-color: #42b983;
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 // Vuetify
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
