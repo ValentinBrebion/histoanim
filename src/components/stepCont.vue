4ef4c6a35717465afa724f21eba14f4a15371786
valentin <vb.brebionpro@gmail.com>
2025-10-26 22:27:31 +0100

    initialisation

---
+++ src/components/stepCont.vue
@@ -0,0 +1,28 @@
+<template>
+   
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const step = ref(0)
+
+const steps = [
+  { label: 'Choose your product' },
+  { label: 'Checkout' },
+  { label: 'Review order' },
+  { label: 'Confirm and pay' },
+]
+</script>
+<style lang="scss">
+$primary-color: #42b983;
+
+.card {
+  background-color: lighten($primary-color, 30%);
+  border-radius: 12px;
+  padding: 20px;
+
+  h1 {
+    color: $primary-color;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import './style.css'
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+import router from './router/router';
+
+
+createApp(App).use(vuetify).use(router).mount("#app");
