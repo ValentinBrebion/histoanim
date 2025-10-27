d5b31d48f4a0e362cf34748f13be274069b75461
valentin <vb.brebionpro@gmail.com>
2025-10-27 22:50:45 +0100

    removve stylke

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 
 // Vuetify
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
