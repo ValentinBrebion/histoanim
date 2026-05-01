da5eec7bfe234a4121fefe417ddd0019e23e26c3
valentin <vb.brebionpro@gmail.com>
2026-05-01 23:03:11 +0200

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,20 @@ import pluginVue from 'eslint-plugin-vue'
 
 export default [
   js.configs.recommended,
-  ...pluginVue.configs['flat/vue3-recommended'],
   {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: pluginVue.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      vue: pluginVue
+    },
     rules: {
+      ...pluginVue.configs.base.rules,
       'vue/multi-word-component-names': 'off',
       'no-unused-vars': 'warn'
     }
