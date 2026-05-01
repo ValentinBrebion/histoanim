eb0e62b28974fd3e4671ffa0033b4fd79d2be8ea
valentin <vb.brebionpro@gmail.com>
2026-05-01 22:54:34 +0200

    fix eslint action

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+  ],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "no-unused-vars": "warn"
+  }
+};
